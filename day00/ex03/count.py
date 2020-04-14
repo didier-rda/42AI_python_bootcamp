ee46614c38251688d7a175a8582f52ad16f03626
didier-rda <didier.rda@gmail.com>
2020-04-14 02:45:39 -0300

    create count.py

---
+++ day00/ex03/count.py
@@ -0,0 +1,15 @@
+def text_analyzer():
+    pass
+
+
+def ft_print_Summary():
+    text = f'The text contains {n_char} characters:\n\
+- {n_up} upper letters\n\
+- {n_low} lower letters\n\
+- {n_punc} punctuation marks\n\
+- {n_spaces} spaces'
+    print(text)
+
+
+if __name__ == '__main__':
+    ft_print_Summary()
